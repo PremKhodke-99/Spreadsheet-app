2d3bf635cb3f95092634c6e632d38418334f37e6
Prem <premkhodke.99@gmail.com>
2024-08-08 20:32:23 +0530

    Navigation bar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='h-14 bg-emerald-600 text-white flex items-center'>
+        <h1 className='text-3xl font-semibold px-8 '>Spreadsheet</h1>
+    </nav>
+  )
+}
+
+export default Navbar
