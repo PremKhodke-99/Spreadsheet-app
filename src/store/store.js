f44ac652c5af848040d40abd1cc5ae68aff34495
Prem <premkhodke.99@gmail.com>
2024-08-09 16:45:47 +0530

    previous & forward states and implementation of new methods

--- src/store/store.js
+++ src/store/store.js
@@ -1,14 +1,50 @@
 const { create } = require("zustand");
 
-const initialState = Array.from({length: 100}, () => new Array(10).fill(''));
+const initialState = Array.from({ length: 100 }, () =>
+  new Array(10).fill({
+    data: "",
+    validation: null,
+    align: "text-left",
+    fontSize: "text-sm",
+  })
+);
 
-export default useStore = create((set) => ({
-    gridCellData: initialState,
-    updateCell: (row, col, data) => {
-        set((state) => {
-            const newGrid = [...state.gridCellData];
-            newGrid[row][col] = data;
-            return {gridCellData: newGrid}
-        })
-    },
-}))
+const useStore = create((set) => ({
+  gridCellData: initialState,
+  previousData: [],
+  forwardData: [],
+
+  updateCellData: (row, col, data) => {
+    set((state) => {
+      const newGrid = [...state.gridCellData];
+      const cell = newGrid[row][col];
+
+      const newPrevious = [...state.previousData, state.gridCellData];
+      newGrid[row][col] = { ...cell, data };
+
+      return {
+        gridCellData: newGrid,
+        previousData: newPrevious,
+        forwardData: [],
+      };
+    });
+  },
+
+  formatData: (row, col, format) => {
+    set((state) => {
+      const newGrid = [...state.gridCellData];
+      newGrid[row][col] = { ...newGrid[row][col], ...format };
+      return { gridCellData: newGrid };
+    });
+  },
+
+  undoData: () => {
+    set((state) => {});
+  },
+
+  redoData: () => {
+    set((state) => {});
+  },
+}));
+
+export default useStore;
