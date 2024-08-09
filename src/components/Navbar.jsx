ddd43a968817d0abd767a89a873e9be435cfef6d
Prem <premkhodke.99@gmail.com>
2024-08-09 16:44:09 +0530

    Responsive layout update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,31 @@
+'use client'
+
+import useStore from '@/store/store'
 import React from 'react'
 
 const Navbar = () => {
-  return (
-    <nav className='h-14 bg-emerald-600 text-white flex items-center'>
-        <h1 className='text-3xl font-semibold px-8 '>Spreadsheet</h1>
-    </nav>
-  )
+
+    const { undoData, redoData } = useStore();
+
+    return (
+        <nav className='h-12 px-8 bg-emerald-600 text-white flex items-center justify-between'>
+            <h1 className='text-lg md:text-xl lg:text-2xl font-semibold'>Spreadsheet</h1>
+            <div className='flex gap-4'>
+                <button
+                    className='border py-1 px-2 rounded-md bg-white text-emerald-600 hover:bg-emerald-600 hover:text-white duration-300'
+                    onClick={undoData}
+                >
+                    Undo
+                </button>
+                <button
+                    className='border py-1 px-2 rounded-md bg-white text-emerald-600 hover:bg-emerald-600 hover:text-white duration-300'
+                    onClick={redoData}
+                >
+                    Redo
+                </button>
+            </div>
+        </nav>
+    )
 }
 
 export default Navbar
